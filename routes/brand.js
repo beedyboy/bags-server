1d1c898479312912542f431af25e5aea22bc0c0a
Bolade Akinniyi <boladebode@gmail.com>
2021-05-21 19:51:33 +0100

    latest update

--- routes/brand.js
+++ routes/brand.js
@@ -3,7 +3,7 @@ const brandController = require("../controller/brand");
 const router = express.Router();
 
 router.post("/", brandController.createBrand);
-router.post("/confirm", brandController.createBrand);
+router.post("/confirm", brandController.exist);
 router.put("/", brandController.updateBrand);
 
 router.get("/", brandController.getAllBrands);
